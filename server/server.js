360bb359f6ae2eb99278c9bc789ef2b14eca52aa
samkulesa <kule2623@sttthomas.edu>
2023-02-28 23:12:51 -0600

    cors fix

--- server/server.js
+++ server/server.js
@@ -31,12 +31,13 @@ app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
-const corsOptions = {
-  origin: 'http://localhost:3000',
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+app.use(cors());
+// const corsOptions = {
+//   origin: 'http://localhost:3000',
+//   credentials: true, //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// };
+// app.use(cors(corsOptions));
 // app.use(cors({
 //   origin: 'http://localhost:3000',
 //   credentials: true
