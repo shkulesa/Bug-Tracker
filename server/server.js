105e411ffcdcabad42ecc76878c0b7e0b987e3e2
samkulesa <kule2623@sttthomas.edu>
2023-03-01 00:28:20 -0600

    cors tweaks again

--- server/server.js
+++ server/server.js
@@ -37,6 +37,7 @@ const corsOptions = {
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
+app.use(cors(corsOptions));
 // app.use(cors(corsOptions));
 // app.use(cors({
 //   origin: 'http://localhost:3000',
